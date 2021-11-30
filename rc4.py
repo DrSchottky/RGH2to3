83a982e41af313c86543e3844c469704c2f8dc86
John Dong <pls@gooby.co>
2021-11-29 20:16:32 -0500

    Fixed indentation in rc4.py

--- rc4.py
+++ rc4.py
@@ -2,10 +2,10 @@
 
 class RC4:
 	"""
-    This class implements the RC4 streaming cipher.
+	This class implements the RC4 streaming cipher.
 
-    Derived from http://cypherpunks.venona.com/archive/1994/09/msg00304.html
-    """
+	Derived from http://cypherpunks.venona.com/archive/1994/09/msg00304.html
+	"""
 
 	def __init__(self, key, streaming=True):
 		assert (isinstance(key, (bytes, bytearray)))
@@ -27,16 +27,16 @@ class RC4:
 
 	def crypt(self, data):
 		"""
-        Encrypts/decrypts data (It's the same thing!)
-        """
+		Encrypts/decrypts data (It's the same thing!)
+		"""
 		assert (isinstance(data, (bytes, bytearray)))
 		keystream = self.keystream or self._keystream_generator()
 		return bytes([a ^ b for a, b in zip(data, keystream)])
 
 	def _keystream_generator(self):
 		"""
-        Generator that returns the bytes of keystream
-        """
+		Generator that returns the bytes of keystream
+		"""
 		S = self.S.copy()
 		x = y = 0
 		while True:
